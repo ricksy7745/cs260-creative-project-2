0614fe1260a3fb1e14411f8c116176e3dcd15161
ricksy7745 <beckricks@gmail.com>
2022-10-21 13:16:26 -0600

    FInal Edit

--- apigetter.js
+++ apigetter.js
@@ -12,7 +12,7 @@ document.getElementById("form").addEventListener("submit", (event) => {
         console.log(json);
         document.getElementById("pokemon-sprite").src=json.sprites.front_default;
         document.getElementById("pokemon-number").innerHTML="<h2>PokeDex Number: " + json.id + "</h2>";
-        let pokemonTypes = "<h2>Pokemon Type";
+        let pokemonTypes = "<h2>Type";
         if(json.types.length > 1) { pokemonTypes += "s"; }
         pokemonTypes += ": "
         for(let i = 0; i < json.types.length; i++) {
@@ -20,14 +20,14 @@ document.getElementById("form").addEventListener("submit", (event) => {
             pokemonTypes += json.types[i].type.name;
         }
         document.getElementById("pokemon-type").innerHTML=pokemonTypes;
-        document.getElementById("pokemon-height").innerHTML="<h2>Pokemon Height: " + json.height + "</h2>";
-        document.getElementById("pokemon-weight").innerHTML="<h2>Pokemon Weight: " + json.weight + "</h2>";
+        document.getElementById("pokemon-height").innerHTML="<h2>Height: " + json.height + "</h2>";
+        document.getElementById("pokemon-weight").innerHTML="<h2>Weight: " + json.weight + "</h2>";
     })
     .catch(function(error) {
-        document.getElementById("pokemon-sprite").src="https://upload.wikimedia.org/wikipedia/commons/c/cf/Question_mk_grey.png";
-        document.getElementById("pokemon-type").innerHTML="<h2>Pokemon Type(s): Not Found</h2>";
-        document.getElementById("pokemon-height").innerHTML="<h2>Pokemon Height: Not Found</h2>";
-        document.getElementById("pokemon-weight").innerHTML="<h2>Pokemon Weight: Not Found</h2>";
         document.getElementById("pokemon-number").innerHTML="<h2>PokeDex Number: Not Found</h2>";
+        document.getElementById("pokemon-sprite").src="https://upload.wikimedia.org/wikipedia/commons/c/cf/Question_mk_grey.png";
+        document.getElementById("pokemon-type").innerHTML="<h2>Type(s): Not Found</h2>";
+        document.getElementById("pokemon-height").innerHTML="<h2>Height: Not Found</h2>";
+        document.getElementById("pokemon-weight").innerHTML="<h2>Weight: Not Found</h2>";
     });
 });
