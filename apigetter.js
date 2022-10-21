55f39ef73ed7ae5dd6ebc949d1b1cf0451075fcd
ricksy7745 <beckricks@gmail.com>
2022-10-20 22:55:02 -0600

    Event Listener is bugged, but format is done

--- apigetter.js
+++ apigetter.js
@@ -1,1 +1,12 @@
-//testing the stuff
+document.getElementById("name-submit").addEventListener("submit", (event) => {
+    event.preventDefault();
+    const name = document.getElementById("name-input").value;
+    const apiUrl = "https://pokeapi.co/api/v2/pokemon/" + name;
+    fetch(apiUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        console.log(json);
+    });
+});
